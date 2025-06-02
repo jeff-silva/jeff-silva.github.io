66b181df6792a161230edc26acd15917a2bab7af
Jeferson Silva <jeferson.i.silva@gmail.com>
2025-06-02 11:49:54 -0300

    build

--- docs/assets/html.1d1844f2.js
+++ docs/assets/html.1d1844f2.js
@@ -1,4 +1,4 @@
-import{m as p}from"./index.5af29ab6.js";import"./entry.7f4ff943.js";import"./useHelper.f71ef28e.js";import"./_commonjsHelpers.725317a4.js";/*!-----------------------------------------------------------------------------
+import{m as p}from"./index.e459c405.js";import"./entry.f51d2f2a.js";import"./useHelper.f71ef28e.js";import"./_commonjsHelpers.725317a4.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.47.0(69991d66135e4a1fc1cf0b1ac4ad25d429866a0d)
  * Released under the MIT license
---
+++ docs/assets/index.1995eb03.js
@@ -0,0 +1,1 @@
+import{u as s}from"./vue.f36acd1f.af7d6b94.js";import{ar as u}from"./entry.f51d2f2a.js";function i(e,a){const{title:r,titleTemplate:o,...m}=e;return s({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const n=u({...t._flatMeta});return delete t._flatMeta,{...t,meta:n}}})}export{i as u};
